9014de40206bb36d9d4ff3fa0481750f4ddb1cd0
Ankur Singh <ankursinghagra@gmail.com>
2024-11-22 16:53:07 +0530

    node js added

--- node_port/db/mysql.js
+++ node_port/db/mysql.js
@@ -13,6 +13,7 @@ connection.connect(()=>{
 });
 
 connection.on('error', err => {
+  console.log(process.env.DB_HOST,process.env.DB_USER,process.env.DB_PASS,process.env.DB_DATABASE)
   if (err.code === 'PROTOCOL_CONNECTION_LOST') {
       // db error reconnect
       //mysql.disconnect_handler();
